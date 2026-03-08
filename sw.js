7a31a8cab6d021039e5439a38e5c1652303aa2da
APP <egon1210@gmail.com>
2026-03-08 20:38:22 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "gedenkportal-v1";
+
+const URLS_TO_CACHE = [
+  "/gedenkportal/",
+  "/gedenkportal/index.html",
+  "/gedenkportal/manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
