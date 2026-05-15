393b037406ebfc2c7f6cc6487a35153f4b9c6cbb
APP <egon1210@gmail.com>
2026-05-15 19:05:52 +0200

    Update sw.js V2

--- sw.js
+++ sw.js
@@ -1,23 +1,22 @@
-const CACHE_NAME = "gedenkportal-v1";
-
+const CACHE_NAME = "gedenkportal-v2";
 const URLS_TO_CACHE = [
-  "/gedenkportal/",
-  "/gedenkportal/index.html",
-  "/gedenkportal/manifest.webmanifest"
+    "/gedenkportal/",
+    "/gedenkportal/index.html",
+    "/gedenkportal/manifest.webmanifest"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(URLS_TO_CACHE);
+        })
+    );
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
